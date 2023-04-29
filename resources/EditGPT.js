407902460582550536a42639eb1ad3305983c9bf
AurLemon <67463076+AurLemon@users.noreply.github.com>
2023-04-29 23:56:27 +0800

    Add files via upload
    更新

--- resources/EditGPT.js
+++ resources/EditGPT.js
@@ -1,8 +1,5 @@
 ( function ( mw, $, OO ) {
     var EditGPT = {
-        // apiBase: mw.config.get('EditGPTAPIBase'),
-        // apiKey: mw.config.get('EditGPTAPIKey'),
-        // apiModel: mw.config.get('EditGPTModel'),
         init: function () {
             if ($('#EditGPTWidget').length) {
                 this.addEventListeners();
@@ -18,18 +15,25 @@
             mw.message('editgpt-button-label').plain() +
             '</button><button id="EditGPTCopy">' +
             mw.message('editgpt-button-copy').plain() +
-            '</button>' +
-            '</div></div>' +
+            '</button><select id="speech">' +
+            '</select></div></div>' +
             '<div id="input"><div id="type-name">'+ mw.message('editgpt-input').plain() +'</div><textarea id="EditGPTInput"></textarea></div>' +
             '<div id="output"><div id="type-name">'+ mw.message('editgpt-output').plain() +'</div><div id="EditGPTOutput"></div></div>' +
-            '<div id="Console"><img id="logo" src="data:image/svg+xml;charset=utf-8;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGhlaWdodD0nNDgnIHZpZXdCb3g9JzAgOTYgOTYwIDk2MCcgd2lkdGg9JzQ4Jz48cGF0aCBkPSdNMTQwIDg5NnEtMjQgMC00Mi0xOHQtMTgtNDJWMzE2cTAtMjQgMTgtNDJ0NDItMThoNjgwcTI0IDAgNDIgMTh0MTggNDJ2NTIwcTAgMjQtMTggNDJ0LTQyIDE4SDE0MFptMC02MGg2ODBWNDAwSDE0MHY0MzZabTE2MC03Mi00Mi00MiAxMDMtMTA0LTEwNC0xMDQgNDMtNDIgMTQ2IDE0Ni0xNDYgMTQ2Wm0xOTAgNHYtNjBoMjIwdjYwSDQ5MFonLz48L3N2Zz4=" /><div id="Console-info">欢迎使用EditGPT。输入您的问题后即可调用ChatGPT。</div>' +
+            '<div id="Console"><img id="logo" src="data:image/svg+xml;charset=utf-8;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGhlaWdodD0nNDgnIHZpZXdCb3g9JzAgOTYgOTYwIDk2MCcgd2lkdGg9JzQ4Jz48cGF0aCBkPSdNMTQwIDg5NnEtMjQgMC00Mi0xOHQtMTgtNDJWMzE2cTAtMjQgMTgtNDJ0NDItMThoNjgwcTI0IDAgNDIgMTh0MTggNDJ2NTIwcTAgMjQtMTggNDJ0LTQyIDE4SDE0MFptMC02MGg2ODBWNDAwSDE0MHY0MzZabTE2MC03Mi00Mi00MiAxMDMtMTA0LTEwNC0xMDQgNDMtNDIgMTQ2IDE0Ni0xNDYgMTQ2Wm0xOTAgNHYtNjBoMjIwdjYwSDQ5MFonLz48L3N2Zz4=" />' +
+            '<img id="error-img" src="data:image/svg+xml;charset=utf-8;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGhlaWdodD0nNDgnIHZpZXdCb3g9JzAgOTYgOTYwIDk2MCcgd2lkdGg9JzQ4Jz48cGF0aCBkPSdtMjQ5IDg0OS00Mi00MiAyMzEtMjMxLTIzMS0yMzEgNDItNDIgMjMxIDIzMSAyMzEtMjMxIDQyIDQyLTIzMSAyMzEgMjMxIDIzMS00MiA0Mi0yMzEtMjMxLTIzMSAyMzFaJy8+PC9zdmc+" />' +
+            '<div id="loader" style="display: none;"><div></div><div></div><div></div><div></div><div></div></div>' +
+            '<div id="Console-info">' +
+            mw.message('editgpt-welcome-info').plain() +
+            '</div>' +
             '</div></div>';
 
             var self = this;
+
             $('#EditGPTWidget').html(htmlText);
             $('#EditGPTButton').on('click', function (e) {
                 e.preventDefault();
                 self.getEditedText();
+                document.getElementById('error-img').style.display = 'none';
             });
             $('#EditGPTCopy').on('click', function (e) {
                 function copyElementContentToClipboard(selector) {
@@ -47,13 +51,34 @@
                   copyElementContentToClipboard("#EditGPTOutput");
                   mw.notify(mw.message('editgpt-copy-success').plain());
             });
+
+            var speechElement = document.getElementById('speech');
+            var speech = mw.config.get('EditGPTSpeech');
+            var speechSelectHTML = '<option selected>' + mw.message('editgpt-speech-customize').plain() + '</option>';
+            $.each(speech, function(index, value) {
+              speechSelectHTML += '<option>' + value['name'] + '</option>';
+            });
+            speechElement.innerHTML = speechSelectHTML;
         },
 
         getEditedText: function () {
             var inputText = $('#EditGPTInput').val();
             if (inputText !== '') {
+                document.getElementById('loader').style.display = 'flex';
                 var buttonOutput = $('#Console-info');
-                buttonOutput.html(mw.message('editgpt-request-sending').plain());
+
+                var speechSelect = document.getElementById("speech");
+                var selectedSpeechIndex = speechSelect.selectedIndex;
+
+                if (selectedSpeechIndex == 0) {
+                    speechSelectedOption = mw.message('editgpt-speech-customize').plain();
+                } else {
+                    var speech = mw.config.get('EditGPTSpeech');
+                    speechSelectedOption = speech[selectedSpeechIndex - 1]['name'];
+                }
+
+                var speech = mw.config.get('EditGPTSpeech');
+                buttonOutput.html(mw.message('editgpt-speech-selected').plain() + ': ' + speechSelectedOption + mw.message('editgpt-completion-period').plain() + mw.message('editgpt-request-sending').plain());
 
                 $.ajax({
                 url: mw.util.wikiScript('api'),
@@ -65,36 +90,50 @@
                     action: 'editgptrequest',
                     format: 'json',
                     inputText: inputText,
-                    Token: mw.config.get('EditGPTSecurityToken')
+                    Token: mw.config.get('EditGPTSecurityToken'),
+                    Speech: selectedSpeechIndex
                 },
                 dataType: 'json'
                 }).done(function(response) {
-                    var editedText = JSON.parse(response.editgptrequest).choices[0].message.content;
-                    var createdTime = JSON.parse(response.editgptrequest).created;
-                    var modelType = JSON.parse(response.editgptrequest).model;
-                    var totalTokens = JSON.parse(response.editgptrequest).usage.total_tokens;
+                    document.getElementById('loader').style.display = 'none';
+                    if ('editgptrequest' in response) {
+                        var editedText = JSON.parse(response.editgptrequest).choices[0].message.content;
+                        var createdTime = JSON.parse(response.editgptrequest).created;
+                        var modelType = JSON.parse(response.editgptrequest).model;
+                        var totalTokens = JSON.parse(response.editgptrequest).usage.total_tokens;
+    
+                        var output = $('#EditGPTOutput');
+                        output.html(editedText);
+    
+                        // var editedTextArray = editedText.split('');
+                        // var i = 0;
+                        // var output = $('#EditGPTOutput');
+                        // output.html('');
+                        // var timer = setInterval(function () {
+                        //     if (i < editedTextArray.length) {
+                        //         output.append(editedTextArray[i]);
+                        //         i++;
+                        //     } else {
+                        //         clearInterval(timer);
+                        //     }
+                        // }, 50);
 
-                    var editedTextArray = editedText.split('');
-                    var i = 0;
-                    var output = $('#EditGPTOutput');
-                    output.html('');
-                    var timer = setInterval(function () {
-                        if (i < editedTextArray.length) {
-                            output.append(editedTextArray[i]);
-                            i++;
-                        } else {
-                            clearInterval(timer);
-                        }
-                    }, 50);
-                    
-                    var consoleOutputSplit = mw.message('editgpt-completion-comma').plain();
-                    var consoleOutput = $('#Console-info');
-                    consoleOutput.html(mw.message('editgpt-completion-created').plain() + ': ' + createdTime + consoleOutputSplit + mw.message('editgpt-completion-totaltoken').plain() + ': ' + totalTokens + consoleOutputSplit + mw.message('editgpt-completion-model').plain() + ': ' + modelType + mw.message('editgpt-completion-period').plain());
+                        var consoleOutputSplit = mw.message('editgpt-completion-comma').plain();
+                        var consoleOutput = $('#Console-info');
+                        consoleOutput.html(mw.message('editgpt-completion-created').plain() + ': ' + createdTime + consoleOutputSplit + mw.message('editgpt-completion-totaltoken').plain() + ': ' + totalTokens + consoleOutputSplit + mw.message('editgpt-completion-model').plain() + ': ' + modelType + mw.message('editgpt-completion-period').plain());
+                    } else if ('error' in response) {
+                        document.getElementById('error-img').style.display = 'block';
+                        var consoleOutput = $('#Console-info');
+                        consoleOutput.html(response.error.info);
+                    }
                 }).fail(function(xhr, textStatus, errorThrown) {
+                    document.getElementById('loader').style.display = 'none';
+                    document.getElementById('error-img').style.display = 'block';
                     var consoleOutput = $('#Console-info');
                     consoleOutput.html(mw.message('editgpt-request-failed').plain());
                 });   
             } else {
+                document.getElementById('loader').style.display = 'none';
                 var output = $('#Console-info');
                 output.html(mw.message('editgpt-request-empty').plain());
             }
